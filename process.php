6dc130695d3199ca18be8b1d03b97c531df43e78
unknown <Tenshi@Nanoha.(none)>
2012-09-04 03:02:13 +1000

    Sherwyn - Table layout added

--- process.php
+++ process.php
@@ -16,7 +16,7 @@
 	$connstring .= "user=zmffjoapewndxl ";
 	$connstring .= "password=r7mnCO1hMRPy2R8ask5BCsd3s7";
 	
-	$databaseconn = pg_connect($connstring);
+	$dbconn = pg_connect($connstring);
 		
 	$long =  $_POST['longitude'];
 	$lat =  $_POST['latitude'];
@@ -28,10 +28,53 @@ var point = new LatLon(<?php echo "$lat, $long"?>);
 			
 var dist = swin.distanceTo(point);
 
-var resulttxt = "Coordingates <?php echo "latitude $lat, longitude $long"?> is " + dist + " kilometres from Swinburne.";
+var resulttxt = "Coordinates <?php echo "latitude $lat, longitude $long"?> is " + dist + " kilometres from Swinburne.";
 
 document.write(resulttxt);
 
 </script>
+<?php
 
+	if (!$dbconn) {
+  echo "An error occured.\n";
+  exit;
+}
+
+$result = pg_query($dbconn, "SELECT * FROM Coords");
+if (!$result) {
+  echo "An error occured.\n";
+  exit;
+}
+?>
+	<table border="0" cellspacing="2" cellpadding="2">
+<tr>
+<td><font face="Arial, Helvetica, sans-serif">Time</font></td>
+<td><font face="Arial, Helvetica, sans-serif">Latitude</font></td>
+<td><font face="Arial, Helvetica, sans-serif">Longitude</font></td>
+<td><font face="Arial, Helvetica, sans-serif">Distance</font></td>
+</tr>
+
+<?php
+while ($row = pg_fetch_row($result)) {
+	$lat = $row[1];
+	$long = $row[0];
+	$timest = $row[2];
+?>
+<script type="text/javascript">
+	var point = new LatLon(<?php echo "$row[1], $row[0]"?>); 
+			
+	var dist = swin.distanceTo(point);
+
+	</script>
+<tr>
+<td><font face="Arial, Helvetica, sans-serif"><?php echo $timest; ?></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><?php echo $lat; ?></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><?php echo $long; ?></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><script type="text/javascript">document.write(dist);</script></font></td>
+</tr>
+}
+
+<?php
+	pg_close($dbconn);
+?>
 </html>
