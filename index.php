b99fb69c73c3f643b7e9f7ecfcdab7eff050c4af
Ryan Zhao <6555985@student.swin.edu.au>
2012-09-08 23:21:58 +1000

    Ryan -add jquery form validation, the range is from -180 to +180

--- index.php
+++ index.php
@@ -4,7 +4,29 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>HIT3311</title>
 <link rel="stylesheet" type="text/css" href="bootstrap.min.css" media="all">
-<script type="text/javascript" src="view.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="http://jzaefferer.github.com/jquery-validation/jquery.validate.js"></script>
+</script>
+
+<script> 
+$(document).ready(function() {
+	$("#myform").validate({
+	rules: {
+		latitude: 
+		{
+		required: true,
+		range: [-180.0, +180.0]
+		},
+		longitude: 
+		{
+		required: true,
+		range: [-180.0, +180.0]
+		}
+	}
+	});
+
+  });
+</script>
 </head>
 
 <body id="main_body" >
@@ -14,14 +36,14 @@
 	<p>Please enter a latitude and a longitude to get distance </p>
 
 	<div id="form_container">	
-		<form id="form_476022" method="post" action="process.php">
+		<form id="myform" method="post" action="process.php">
 			<p>
-			<label class="description" for="element_1">Latitude </label>
-			<input id="element_1" name="latitude"  type="text" value=""/> 
+			<label for="latitude">Latitude </label>
+			<input id="latitude" name="latitude"  type="text" /> 
 			</p>		
 			<p>
-			<label class="description" for="element_2">Longitude </label>
-			<input id="element_2" name="longitude"  type="text" value=""/> 
+			<label for="longitude">Longitude </label>
+			<input id="longitude" name="longitude"  type="text" /> 
 			</p>		
 			<p>
 			<input id="saveForm" type="submit" name="submit" value="Calculate" />
@@ -32,6 +54,7 @@
 	<div>
 		<h1>Version 1</h1>
 		<p> By 6555985 and 6595979 </p>
+		
 	</div>
 
 </body>
